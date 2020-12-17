120ef8f03ff55600d5085a35c04b3d431ffea57f
smirok <smirok123@mail.ru>
2020-12-18 01:26:44 +0300

    implement lambda calculus operations

--- lambda-calculator/src/LambdaCalculus.hs
+++ lambda-calculator/src/LambdaCalculus.hs
@@ -1,5 +1,9 @@
 module LambdaCalculus where
 
+import Text.ParserCombinators.Parsec
+import Data.List (union, maximumBy)
+import Data.Function (on)
+
 type Symb = String 
 
 infixl 2 :@
@@ -7,4 +11,69 @@ infixl 2 :@
 data Expr = Var Symb
           | Expr :@ Expr
           | Lam Symb Expr
-          deriving (Eq, Read, Show)
+          deriving (Eq)
+
+instance Show Expr where
+    show (Var sym)        = sym
+    show (lTerm :@ rTerm) = "(" ++ show lTerm ++ ") (" ++ show rTerm ++ ")"
+    show (Lam sym expr)   = "\\" ++ sym ++ " -> (" ++ show expr ++ ")" 
+
+instance Read Expr where
+  readsPrec _ str = case runParser parseExpr () "" str of
+      Right x -> [(x,"")]
+      Left _  -> undefined
+
+freeVars :: Expr -> [Symb]
+freeVars (Var sym)      = [sym]  
+freeVars (lTerm :@ rTerm)       = freeVars lTerm `union` freeVars rTerm
+freeVars (Lam sym expr) = filter (sym /=) $ freeVars expr 
+
+subst :: Symb -> Expr -> Expr -> Expr 
+subst var newTerm (Var sym) 
+    | sym == var  = newTerm
+    | otherwise   = Var sym     
+subst var newTerm (lTerm :@ rTerm) = subst var newTerm lTerm :@ subst var newTerm rTerm
+subst var newTerm oldTerm@(Lam sym expr) 
+    | sym == var                     = oldTerm
+    | notElem sym $ freeVars newTerm = Lam sym (subst var newTerm expr)
+    | otherwise                      = Lam newSym (subst var newTerm newExpr)
+    where
+        newSym  = maximumBy (compare `on` length) (freeVars newTerm `union` freeVars expr)
+        newExpr = subst sym (Var newSym) expr
+
+
+infix 1 `alphaEq`
+
+alphaEq :: Expr -> Expr -> Bool
+alphaEq (Var lSym) (Var rSym)               = lSym == rSym
+alphaEq (lTerm :@ rTerm) (lTerm' :@ rTerm') = (lTerm `alphaEq` lTerm') && (rTerm `alphaEq` rTerm')
+alphaEq (Lam lSym lExpr) (Lam rSym rExpr) 
+    | lSym == rSym                  = lExpr `alphaEq` rExpr
+    | notElem lSym $ freeVars rExpr = lExpr `alphaEq` subst rSym (Var lSym) rExpr
+    | otherwise                     = False
+alphaEq _ _ = False
+
+
+reduceOnce :: Expr -> Maybe Expr
+reduceOnce (Lam sym expr :@ term) = Just (subst sym term expr)
+reduceOnce (Lam sym m)  = case reduceOnce m of
+    Nothing    -> Nothing
+    Just expr  -> Just (Lam sym expr)
+reduceOnce (Var _) = Nothing
+reduceOnce (lTerm :@ rTerm) = case reduceOnce lTerm of
+    Just x -> Just (x :@ rTerm)
+    Nothing -> case reduceOnce rTerm of
+        Nothing -> Nothing
+        Just x -> Just (lTerm :@ x)
+
+nf :: Expr -> Expr 
+nf expr = case reduceOnce expr of
+    Nothing    -> expr
+    Just notNF -> nf notNF
+
+infix 1 `betaEq`
+
+betaEq :: Expr -> Expr -> Bool 
+betaEq lExpr rExpr = nf lExpr `betaEq` nf rExpr
+
+parseExpr = undefined
--- lambda-calculator/src/Lib.hs
+++
@@ -1,3 +0,0 @@
-module Lib
-    ( someFunc
-    ) where
