1673f46a30d8320163405daa7445c0dd6f0d98f9
smirok <smirok123@mail.ru>
2020-12-21 01:30:35 +0300

    add instance Show Expr & reduce imports

--- lambda-calculator/src/LambdaCalculus.hs
+++ lambda-calculator/src/LambdaCalculus.hs
@@ -1,6 +1,16 @@
 module LambdaCalculus where
 
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec (
+    Parser(..),
+    many1,
+    spaces,
+    (<|>),
+    sepBy1,
+    sepEndBy1,
+    char,
+    string,
+    alphaNum,
+    runParser)
 import Data.List (union, maximumBy)
 import Data.Function (on)
 
@@ -14,19 +24,21 @@ data Expr = Var Symb
           deriving (Eq)
 
 instance Show Expr where
-    show (Var sym)        = sym
-    show (lTerm :@ rTerm) = "(" ++ show lTerm ++ ") (" ++ show rTerm ++ ")"
-    show (Lam sym expr)   = "\\" ++ sym ++ " -> (" ++ show expr ++ ")" 
+    showsPrec _ (Var sym)         = showString sym
+    showsPrec _ (expr :@ Var sym) = shows expr . showChar ' ' . showString sym
+    showsPrec _ (Var sym :@ expr) = showString sym  . showChar ' ' . showChar '(' . shows expr . showChar ')'
+    showsPrec _ (Lam sym expr)    = showChar '\\' . showString sym . showString " -> " . shows expr
+    showsPrec _ (lTerm :@ rTerm)  = showChar '(' . shows lTerm . showString ") (" . shows rTerm . showChar ')'
 
 instance Read Expr where
   readsPrec _ str = case runParser parseExpr () "" str of
       Right x -> [(x,"")]
-      Left _  -> undefined
+      Left _  -> []
 
 freeVars :: Expr -> [Symb]
-freeVars (Var sym)      = [sym]  
-freeVars (lTerm :@ rTerm)       = freeVars lTerm `union` freeVars rTerm
-freeVars (Lam sym expr) = filter (sym /=) $ freeVars expr 
+freeVars (Var sym)        = [sym]  
+freeVars (lTerm :@ rTerm) = freeVars lTerm `union` freeVars rTerm
+freeVars (Lam sym expr)   = filter (sym /=) $ freeVars expr 
 
 subst :: Symb -> Expr -> Expr -> Expr 
 subst var newTerm (Var sym) 
@@ -40,7 +52,6 @@ subst var newTerm oldTerm@(Lam sym expr)
     where
         newSym  = maximumBy (compare `on` length) (freeVars newTerm `union` freeVars expr)
         newExpr = subst sym (Var newSym) expr
-
 
 infix 1 `alphaEq`
 
@@ -57,14 +68,14 @@ alphaEq _ _ = False
 reduceOnce :: Expr -> Maybe Expr
 reduceOnce (Lam sym expr :@ term) = Just (subst sym term expr)
 reduceOnce (Lam sym m)  = case reduceOnce m of
-    Nothing    -> Nothing
-    Just expr  -> Just (Lam sym expr)
+    Nothing   -> Nothing
+    Just expr -> Just (Lam sym expr)
 reduceOnce (Var _) = Nothing
 reduceOnce (lTerm :@ rTerm) = case reduceOnce lTerm of
-    Just x -> Just (x :@ rTerm)
+    Just x  -> Just (x :@ rTerm)
     Nothing -> case reduceOnce rTerm of
         Nothing -> Nothing
-        Just x -> Just (lTerm :@ x)
+        Just x  -> Just (lTerm :@ x)
 
 nf :: Expr -> Expr 
 nf expr = case reduceOnce expr of
@@ -76,4 +87,28 @@ infix 1 `betaEq`
 betaEq :: Expr -> Expr -> Bool 
 betaEq lExpr rExpr = nf lExpr `betaEq` nf rExpr
 
-parseExpr = undefined
+parseVariable :: Parser String
+parseVariable = spaces >> many1 alphaNum
+
+parseVar :: Parser Expr
+parseVar = Var <$> parseVariable    
+
+parseLambda :: Parser Expr
+parseLambda = do
+    _    <- char '\\' >> spaces
+    vars <- sepEndBy1 parseVariable spaces
+    _    <- string "->" >> spaces
+    expr <- parseExpr
+    return $ foldr Lam expr vars
+
+parseBracketsExpr :: Parser Expr
+parseBracketsExpr = do
+    _ <- char '(' >> spaces
+    expr <- parseExpr
+    _ <- char ')' >> spaces
+    return expr
+
+parseExpr :: Parser Expr
+parseExpr = do
+    exprs <- sepBy1 (parseBracketsExpr <|> parseLambda <|> parseVar) spaces
+    return $ foldl1 (:@) exprs
